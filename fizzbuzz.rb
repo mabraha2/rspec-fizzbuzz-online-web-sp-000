ad33350457bd45f73f9b463b890f1771db33d3fb
Medhanei  Abraha <abrahama2019@gmail.com>
2021-01-15 06:01:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 15 == 3
 fizz_3 = "Fizz"
   return fizz_3
 elsif
-   int % 5 == 0
+   int % 15 == 5
     fizz_5 = "Buzz"
     return fizz_5
   else
-      int % 15 == 5
+      int % 5 == 0
       fizz_15 = "FizzBuzz"
       return fizz_15
   end
