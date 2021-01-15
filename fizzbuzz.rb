6e6996f45816af3b757e45cf5c5f8cb0747f9d04
Medhanei  Abraha <abrahama2019@gmail.com>
2021-01-15 06:06:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,20 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 15 == 3
+fizz_3 = "Fizz"
+  return fizz_3
+elsif
+   int % 15 == 5
+    fizz_5 = "Buzz"
+    return fizz_5
+  elsif
+    int % 4 == 0
+    fizz_4 = nil
+    return fizz_4
+  else
+      int % 5 == 4
+      fizz_15 = "FizzBuzz"
+      return fizz_15
+  end
+end
+
+fizzbuzz(3)
